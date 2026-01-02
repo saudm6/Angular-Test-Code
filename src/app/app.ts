154740697e733e73ce7ad0b6331629b3e9214050
Saud Al Maawali <soud111mawali@gmail.com>
2026-01-02 22:06:11 +0400

    Fixed minimal code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Dialog } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Dialog, ButtonModule, InputTextModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  standalone: true,
 })
 export class App {
-  protected readonly title = signal('test_dialog');
+  visible: boolean = false;
+  showDialog() {
+      this.visible = true;
+  }
 }
