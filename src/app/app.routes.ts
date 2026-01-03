8b1538ab9a335f38bb92a981b588ffba6e988acb
Saud Al Maawali <soud111mawali@gmail.com>
2026-01-03 10:56:37 +0400

    Fixed simple pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{
+    path: 'home-page',
+    loadComponent: () => import('./pages/home-page/home-page.ts').then(m => m.HomePage)
+}
+
+
+
+
+];
