f672da1f3b46f28dcaae4cbd9e2e4b2930b69404
Saud Al Maawali <soud111mawali@gmail.com>
2026-01-03 10:56:00 +0400

    Readded simple pages

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,11 @@
 import { Component } from '@angular/core';
-import { Dialog } from 'primeng/dialog';
-import { ButtonModule } from 'primeng/button';
-import { InputTextModule } from 'primeng/inputtext';
-
-
 
 @Component({
   selector: 'app-root',
-  imports: [Dialog, ButtonModule, InputTextModule],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css',
   standalone: true,
 })
 export class App {
-  visible: boolean = false;
-  showDialog() {
-      this.visible = true;
-  }
 }
